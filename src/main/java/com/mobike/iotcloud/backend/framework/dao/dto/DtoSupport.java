b07a1e47c8d8710a27df7961e3fe530c48060275
路永召 <luyongzhao@mobike.com>
2018-04-18 10:55:08 +0800

    解决不能运行的bug

--- src/main/java/com/mobike/iotcloud/backend/framework/dao/dto/DtoSupport.java
+++ src/main/java/com/mobike/iotcloud/backend/framework/dao/dto/DtoSupport.java
@@ -1,14 +1,11 @@
 package com.mobike.iotcloud.backend.framework.dao.dto;
 
-import org.codehaus.jackson.map.annotate.JsonSerialize;
-import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
 import java.io.Serializable;
 
 /**
  * @author luyongzhao
  */
-@JsonSerialize(include = Inclusion.NON_NULL)
 public interface DtoSupport extends Serializable {
 
 }
